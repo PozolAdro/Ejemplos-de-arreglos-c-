8bf34715d49d6c8672a5ccd8f1de39f69f7f5be5
Adrian <Adrian@DESKTOP-COFL36B>
2023-10-06 00:14:35 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplos de arreglos c/Ejemplos de arreglos c.cpp
@@ -0,0 +1,19 @@
+//AdriS
+#include <iostream>
+
+using namespace std; 
+int main() {
+    int numeros[6] = { 10, 20, 30, 40, 50, 60 };
+    cout << numeros[5] << endl;
+
+    int n[3] = { 3, 4, 5 };
+    cout << n[2] << endl;
+
+    char c[] = { 'L', 'u', 'i', 's', '\0' };
+    cout << c << endl;
+
+    char s[] = "Mona Lisa";
+    cout << s << endl;
+
+    return 0; 
+}
